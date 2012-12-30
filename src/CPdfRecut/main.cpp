balancap/PdfRecut

commit 83813919b2bfab737c4e7eb2f01a6836d9ef9c70

Fix bug when reading glyphs widths for Type 0 fonts.
    Add function PdfeIndirectObject that resolve indirect objects (represented by their reference).
    Situation occured in widths array for Type 0 fonts.
    Eg: Crisis.pdf

src/libPoDoFoExtended/PdfeFontType0.cpp
@@ -205,12 +205,7 @@ void PdfeFontCID::init( PoDoFo::PdfObject* pFont )
 
     // Read width of glyphs CID.
     double defaultWidth = static_cast<double>( pFont->GetDictionary().GetKeyAsLong( "DW", 1000L ) );
-    if( pWidths ) {
-        m_hBBoxes.init( pWidths->GetArray(), defaultWidth, m_fontDescriptor.fontBBox() );
-    }
-    else {
-        m_hBBoxes.init( PdfArray(), defaultWidth, m_fontDescriptor.fontBBox() );
-    }
+    m_hBBoxes.init( pWidths, defaultWidth, m_fontDescriptor.fontBBox() );
 }
 void PdfeFontCID::initCharactersBBox( FT_Face ftFace )
 {
@@ -297,18 +292,22 @@ void PdfeFontCID::HBBoxArray::init()
     m_defaultAdvance = PdfeVector( 1000., 0. );
     m_defaultBBox = PdfRect( 0., 0., 1000., 500. );
 }
-void PdfeFontCID::HBBoxArray::init( const PdfArray& widths,
+void PdfeFontCID::HBBoxArray::init( const PdfObject* pWidths,
                                     double defaultWidth,
                                     const PdfRect& fontBBox )
 {
     this->init();
-
     // Default height used (from fontBBox) and default bounding box.
     double defaultHeight = fontBBox.GetHeight()+fontBBox.GetBottom();
     m_defaultAdvance = PdfeVector( defaultWidth, 0. );
     m_defaultBBox = PdfRect( 0., 0., defaultWidth, defaultHeight );
 
+    if( !pWidths || !pWidths->IsArray() ) {
+        return;
+    }
     size_t i = 0;
+    const PdfObject* pObj;
+    const PdfArray& widths = pWidths->GetArray();
     while( i < widths.size() ) {
         // Increase size of vectors.
         m_firstCID.push_back( 0 );
@@ -317,12 +316,14 @@ void PdfeFontCID::HBBoxArray::init( const PdfArray& widths,
         m_advanceCID.push_back( std::vector<PdfeVector>() );
 
         // First CID value.
-        m_firstCID.back() = static_cast<pdfe_cid>( widths[i].GetNumber() );
+        pObj = PdfeIndirectObject( &widths[i], pWidths->GetOwner() );
+        m_firstCID.back() = static_cast<pdfe_cid>( pObj->GetNumber() );
         ++i;
 
         // Read array of widths.
-        if( widths[i].IsArray() ) {
-            const PdfArray& widthsCID = widths[i].GetArray();
+        pObj = PdfeIndirectObject( &widths[i], pWidths->GetOwner() );
+        if( pObj->IsArray() ) {
+            const PdfArray& widthsCID = pObj->GetArray();
 
             // Resize advance bbox vector with default height set.
             m_advanceCID.back().resize( widthsCID.size(),
@@ -340,14 +341,14 @@ void PdfeFontCID::HBBoxArray::init( const PdfArray& widths,
         }
         // Read width for a range of CIDs.
         else {
-            m_lastCID.back() = static_cast<pdfe_cid>( widths[i].GetNumber() );
+            m_lastCID.back() = static_cast<pdfe_cid>( pObj->GetNumber() );
             ++i;
 
             // Create advance vector and bounding box for each glyph.
             m_advanceCID.back().resize( m_lastCID.back()-m_firstCID.back()+1,
-                                        PdfeVector( widths[i].GetReal(), 0. ) );
+                                        PdfeVector( pObj->GetReal(), 0. ) );
             m_bboxCID.back().resize( m_lastCID.back()-m_firstCID.back()+1,
-                                     PdfRect( 0., 0., widths[i].GetReal(), defaultHeight ) );
+                                     PdfRect( 0., 0., pObj->GetReal(), defaultHeight ) );
             ++i;
         }
     }

src/libPoDoFoExtended/PdfeFont.h
@@ -114,11 +114,9 @@ public:
      * \param ftLibrary FreeType library.
      */
     PdfeFont( PdfeFont14Standard::Enum stdFontType, FT_Library ftLibrary );
-
     /** Initialize the object to default parameters.
      */
     void init();
-
     /** Virtual destructor.
      */
     virtual ~PdfeFont();

src/libPoDoFoExtended/PdfeFontType0.h
@@ -212,11 +212,11 @@ protected:
         void init();
         /** Initialize given a PdfArray from a PdfFont object,
          * a FreeType font face and the default font bounding box.
-         * \param widths PdfArray containing CID and widths informations.
+         * \param pWidths PdfArray containing CID and widths informations.
          * \param defWidth Default width.
          * \param fontBBox Font bounding box, used for default height.
          */
-        void init( const PoDoFo::PdfArray& widths,
+        void init( const PoDoFo::PdfObject* pWidths,
                    double defaultWidth,
                    const PoDoFo::PdfRect& fontBBox );
 

src/libPoDoFoExtended/PdfeTypes.cpp
@@ -248,7 +248,6 @@ PoDoFo::PdfRect PdfeORect::intersection(const PoDoFo::PdfRect& lhs,
                              rhs.GetLeft() + rhs.GetWidth() );
     double top = std::min( lhs.GetBottom() + lhs.GetHeight(),
                            rhs.GetBottom() + rhs.GetHeight() );
-
     return PoDoFo::PdfRect( left, bottom,
                             std::max( 0.0, right - left ),
                             std::max( 0.0, top - bottom ) );
@@ -261,7 +260,6 @@ PoDoFo::PdfRect PdfeORect::reunion(const PoDoFo::PdfRect &lhs, const PoDoFo::Pdf
                              rhs.GetLeft() + rhs.GetWidth() );
     double top = std::max( lhs.GetBottom() + lhs.GetHeight(),
                            rhs.GetBottom() + rhs.GetHeight() );
-
     return PoDoFo::PdfRect( left, bottom,
                             right - left,
                             top - bottom );

src/libPoDoFoExtended/PdfeUtils.h
@@ -15,8 +15,40 @@
 #include <cstdlib>
 #include <functional>
 
-// File defining misc classes that can be useful...
+#include <podofo/base/PdfObject.h>
+#include <podofo/base/PdfVecObjects.h>
+
+// File defining misc classes/functions that can be useful...
 namespace PoDoFoExtended {
+
+/** Get the indirect reference of a PoDoFo object. I.e. check if the object
+ * is a reference, which is resolved in that case.
+ * \param pObj Pointer to the object.
+ * \return Pointer to the indirect object.
+ */
+inline const PoDoFo::PdfObject* PdfeIndirectObject( const PoDoFo::PdfObject* pObj,
+                                                    const PoDoFo::PdfVecObjects* pOwner = NULL )
+{
+    if( pObj->IsReference() ) {
+        pOwner = ( pObj->GetOwner() ? pObj->GetOwner() : pOwner );
+        if( !pOwner ) {
+            PODOFO_RAISE_ERROR_INFO( PoDoFo::ePdfError_InvalidHandle,
+                                     "Object is a reference but does not have an owner!" );
+        }
+        while( pObj->IsReference() ) {
+            pObj = pOwner->GetObject( pObj->GetReference() );
+        }
+        return pObj;
+    }
+    return pObj;
+}
+inline PoDoFo::PdfObject* PdfeIndirectObject( PoDoFo::PdfObject* pObj,
+                                              const PoDoFo::PdfVecObjects* pOwner = NULL )
+{
+    return const_cast<PoDoFo::PdfObject*>(
+                PdfeIndirectObject( const_cast<PoDoFo::PdfObject*>( pObj ), pOwner ) );
+}
+
 
 /** Delete functional object. Delete the object and set the pointer to NULL.
  * Example : std::for_each( foobar.begin(), foobar.end(), delete_ptr_fctor<int>() );

commit 7a58f16a57c1a8271b48b084e05b547252754263

Fix bug introduced in commit 83813919b2bfab737c4e7eb2f01a6836d9ef9c70 (already fixing a bug on Type 0 fonts).
Index mistake in width array reading for Type 0 fonts. Occur in example.pdf file.

src/CPdfRecut/main.cpp
@@ -175,8 +175,8 @@ void proceedFile( QString filePath )
 
         // Render some elements.
         renderPage.initRendering( renderParams.resolution );
-        textPage->renderGroupsWords( renderPage, true );
-//        textPage->renderLines( renderPage, false, false );
+//        textPage->renderGroupsWords( renderPage, true );
+        textPage->renderLines( renderPage, false, false );
 //        renderPage.renderElements( renderParams );
 
         // Save image to file.

src/libPoDoFoExtended/PdfeFontType0.cpp
@@ -340,6 +340,7 @@ void PdfeFontCID::HBBoxArray::init( const PdfObject* pWidths,
             ++i;
 
             // Create advance vector and bounding box for each glyph.
+            pObj = PdfeIndirectObject( &widths[i], pWidths->GetOwner() );
             m_advanceCID.back().resize( m_lastCID.back()-m_firstCID.back()+1,
                                         PdfeVector( pObj->GetReal(), 0. ) );
             m_bboxCID.back().resize( m_lastCID.back()-m_firstCID.back()+1,