ff96ef17fdfc93445446abfa37d23a65ff64c3ed
Paul Balança <paul.balanca@gmail.com>
2013-04-07 18:13:26 +0200

    Add PdfePathDevice and PdfeBoostStream.
    Following the boost::iostreams documentation, the device's member
    functions are integrated in a separate class PdfeDataDevice.
    
    PdfeBoostStream is added as a subclass of boost::iostreams, where
    text formatting is initialized to comply PDF reference (i.e. fixed
    floating precision and english locale).
    It is specialized into PdfeDataStream when the device corresponds to
    a PdfeDataDevice object.

--- src/libPoDoFoExtended/PdfeData.cpp
+++ src/libPoDoFoExtended/PdfeData.cpp
@@ -13,9 +13,15 @@
 
 #include <algorithm>
 
+namespace io = boost::iostreams;
+
 namespace PoDoFoExtended {
 
-PdfeData::PdfeData( const char* s ) {
+//**********************************************************//
+//                          PdfeData                        //
+//**********************************************************//
+PdfeData::PdfeData( const char* s )
+{
     this->assign( s, s+std::strlen(s) );
 }
 PdfeData::PdfeData( const char* s, size_t n )
@@ -26,5 +32,57 @@ PdfeData::PdfeData( const std::string& str )
 {
     this->assign( str.begin(), str.end() );
 }
+
+//**********************************************************//
+//                       PdfeDataDevice                     //
+//**********************************************************//
+std::streamsize PdfeDataDevice::read( char* s, std::streamsize n )
+{
+    std::streamsize amt = static_cast<std::streamsize>( m_data.size() - m_pos );
+    std::streamsize result = std::min( n, amt );
+    if( result != 0 ) {
+        std::copy( m_data.begin() + m_pos,
+                   m_data.begin() + m_pos + result,
+                   s );
+        m_pos += result;
+        return result;
+    } else {
+        return -1; // EOF
+    }
+}
+std::streamsize PdfeDataDevice::write( const char* s, std::streamsize n )
+{
+    size_type nsize = m_pos + n;
+    if( nsize > m_data.size() ) {
+        m_data.resize( nsize, 0 );
+    }
+    std::copy( s, s + n, m_data.begin() + m_pos );
+    m_pos += n;
+    return n;
+}
+io::stream_offset PdfeDataDevice::seek( io::stream_offset off, std::ios_base::seekdir way )
+{
+    // Determine new value of m_pos.
+    io::stream_offset next;
+    if( way == std::ios_base::beg ) {
+        next = off;
+    }
+    else if( way == std::ios_base::cur ) {
+        next = m_pos + off;
+    }
+    else if( way == std::ios_base::end ) {
+        next = m_data.size() + off;
+    }
+    else {
+        throw std::ios_base::failure( "<PdfeData> Bad seek direction." );
+    }
+    // Check for errors.
+    if( next < 0 || next > static_cast<io::stream_offset>( m_data.size() ) ) {
+        throw std::ios_base::failure( "<PdfeData> Bad seek offset." );
+    }
+    m_pos = next;
+    return m_pos;
+}
+
 
 }
--- src/libPoDoFoExtended/PdfeData.h
+++ src/libPoDoFoExtended/PdfeData.h
@@ -17,10 +17,19 @@
 #include <cstring>
 #include <ostream>
 
+#include <iosfwd>
+#include <boost/iostreams/categories.hpp>
+#include <boost/iostreams/positioning.hpp>
+#include <boost/iostreams/stream.hpp>
+
 namespace PoDoFoExtended {
 
+//**********************************************************//
+//                          PdfeData                        //
+//**********************************************************//
 /** Basic class used for storage of binary data.
- * Based on std::vector<char>.
+ * Based on std::vector<char>. A few functionalities
+ * are added to the container interface for simplicity of use.
  */
 class PdfeData : private std::vector<char>
 {
@@ -39,8 +48,6 @@ public:
     using std::vector<char>::size_type;
 
     // Use std::vector interface.
-    using std::vector<char>::operator=;
-
     using std::vector<char>::begin;
     using std::vector<char>::end;
     using std::vector<char>::rbegin;
@@ -82,6 +89,34 @@ public:
     PdfeData( const char* s, size_t n );
     PdfeData( const std::string& str );
 
+    // Operator=.
+    PdfeData& operator=( const PdfeData& data ) {
+        this->std::vector<char>::operator=( data );
+        return *this;
+    }
+    PdfeData& operator=( const char* s ) {
+        this->assign( s, s+std::strlen(s) );
+        return *this;
+    }
+    PdfeData& operator=( const std::string& str ) {
+        this->assign( str.begin(), str.end() );
+        return *this;
+    }
+
+    // Operator<<, used to append data.
+    PdfeData& operator<<( const PdfeData& data ) {
+        this->insert( this->end(), data.begin(), data.end() );
+        return *this;
+    }
+    PdfeData& operator<<( const std::string& str ) {
+        this->insert( this->end(), str.begin(), str.end() );
+        return *this;
+    }
+    PdfeData& operator<<( const char* s ) {
+        this->insert( this->end(), s, s+std::strlen(s) );
+        return *this;
+    }
+
 public:
     // A few supplementary member functions.
     const char* data() const {
@@ -97,15 +132,6 @@ public:
     void to_string( std::string& str ) const {
         str.assign( this->data(), this->size() );
     }
-    // Operator<<
-    PdfeData& operator<<( const PdfeData& data ) {
-        this->insert( this->end(), data.begin(), data.end() );
-        return *this;
-    }
-    PdfeData& operator<<( const std::string& str ) {
-        this->insert( this->end(), str.begin(), str.end() );
-        return *this;
-    }
 };
 
 inline std::ostream& operator<<( std::ostream& os, const PdfeData& data ) {
@@ -116,7 +142,113 @@ inline std::string to_string( const PdfeData& data ) {
     return data.to_string();
 }
 
+//**********************************************************//
+//                       PdfeDataDevice                     //
+//**********************************************************//
+/** Implement the idiom Device described in boost::iostreams.
+ * It describes a SeekableDevice based on PdfeData object.
+ * Mainly inspired by the example given in the documentation.
+ */
+class PdfeDataDevice
+{
+public:
+    // Boost::iostreams device.
+    typedef PdfeData::value_type    char_type;
+    typedef boost::iostreams::seekable_device_tag     category;
 
+    /** Default constructor.
+     * \param data Reference to the data buffer used by the device.
+     */
+    PdfeDataDevice( PdfeData& data ) :
+        m_data( data ), m_pos( 0 ) { }
+
+public:
+    // Seekable device interface.
+    /** Read in the data at the current position.
+     * \param s Buffer where to store the data read.
+     * \param n Number of bytes to read.
+     * \return Number of bytes read (-1 if EOF).
+     */
+    std::streamsize read( char* s, std::streamsize n );
+    /** Write data at the current position.
+     * \param s Data to write down.
+     * \param n Number of bytes to write.
+     * \return Number of bytes written.
+     */
+    std::streamsize write( const char* s, std::streamsize n );
+    /** Seek to position off.
+     * \param off Position.
+     * \param way Interpretation of off:
+     *    - std::ios_base::beg indicates an offset from the
+     *     sequence beginning;
+     *    - std::ios_base::cur indicates an offset from the
+     *      current character position;
+     *    - std::ios_base::end indicates an offset from the
+     *      sequence end.
+     */
+    boost::iostreams::stream_offset seek( boost::iostreams::stream_offset off,
+                                          std::ios_base::seekdir way );
+
+private:
+    typedef PdfeData::size_type   size_type;
+    /// Associated data object.
+    PdfeData&  m_data;
+    /// Position in the io data device.
+    size_type  m_pos;
+};
+
+//**********************************************************//
+//                       PdfeBoostStream                    //
+//**********************************************************//
+/** Sub-class of boost stream, specialized to handle
+ * PDF contents (i.e. fixed locale and fixed notations).
+ */
+template< typename Device>
+class PdfeBoostStream : public boost::iostreams::stream<Device>
+{
+public:
+    /// Default constructor.
+    PdfeBoostStream() :
+        boost::iostreams::stream<Device>() {
+        this->init();
+    }
+    /// Construct from a device.
+    PdfeBoostStream( const Device& device ) :
+        boost::iostreams::stream<Device>( device ) {
+        this->init();
+    }
+private:
+    /// Initialize stream parameters.
+    void init();
+};
+
+template< typename Device>
+void PdfeBoostStream<Device>::init()
+{
+    // Float precision.
+    this->setf( std::ios::fixed, std:: ios::floatfield );
+    this->precision( 8 );
+    // Set locale.
+    static const std::locale pdflocale( "C" );
+    this->imbue( pdflocale );
+
+//    try {
+//        s.imbue( cachedLocale );
+//    } catch (const std::runtime_error & e) {
+//        std::ostringstream s;
+//        s << "Failed to set safe locale on stream being used for PDF I/O.";
+//        s << "Locale set was: \"" << PdfIOLocale << "\".";
+//        s << "Error reported by STL std::locale: \"" << e.what() << "\"";
+//        // The info string is copied by PdfError so we're ok to just:
+//        PODOFO_RAISE_ERROR_INFO(
+//                    ePdfError_InvalidDeviceOperation,
+//                    s.str().c_str()
+//                    );
+//    }
+}
+
+/// Stream specialization with PdfeData.
+typedef PdfeBoostStream<PdfeDataDevice>  PdfeDataStream;
 
 }
 
